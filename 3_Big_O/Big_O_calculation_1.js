69894af39b25425480ef0973a826935af40b4b45
chiazam <54377599+chiazam@users.noreply.github.com>
2023-08-09 18:34:10 +0100

    Create Big_O_calculation_1.js

---
+++ 3_Big_O/Big_O_calculation_1.js
@@ -0,0 +1,21 @@
+let funChallenge = (input => {
+
+    let a = 10; // O(1);
+
+    a = 50 + 3;// O(1);
+
+    for (let i = 0; i < input.length; i++) {// O(n);
+
+        anotherFunction();// O(n);
+
+        let stranger = true;// O(n);
+
+        a++;// O(n);
+
+    }
+
+    return a;// O(1);
+
+});
+
+funChallenge(); // BIG O(3 + 4n);
