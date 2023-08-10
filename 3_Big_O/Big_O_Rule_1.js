8b20f187b55e7ebae68b7f0cd54135a1ede1a51f
chiazam <54377599+chiazam@users.noreply.github.com>
2023-08-10 07:51:02 +0100

    Big O Rules 1 and 2

---
+++ 3_Big_O/Big_O_Rule_1.js
@@ -0,0 +1,33 @@
+const azamine = ['Azamine'];
+
+const everyone = ['Emma', "Uche", "Cynthia", "Ozioma", "Caleb", "Joseph"];
+
+const large = new Array(10000).fill('Azamine');
+
+let findAzamine = (arr => {
+
+    let t0 = performance.now();
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] === "Azamine") {
+
+            console.log(`found ${arr[i]}!`);
+
+            break;
+
+        }
+
+    }
+
+    let t1 = performance.now();
+
+    console.log(`Finding Azamine Took ${t1 - t0} Milisecond!`);
+
+});
+
+findAzamine(azamine);
+
+findAzamine(everyone);
+
+findAzamine(large);
---
+++ 3_Big_O/Simplify_Big_O_and_Rules.md
@@ -0,0 +1,10 @@
+# Simplifying Big O?
+
+Big O is determined by a set of rules...
+
+### List of rules
+
+1.  Worst Case: Big O only considers the worst case scenerios.
+2.  Remove Constants
+3.  Different terms for inputs
+4.  Drop Non Dominants
