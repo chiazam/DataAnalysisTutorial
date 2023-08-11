d3a057d4650cb0207d350ed8b63431c02cca2549
chiazam <54377599+chiazam@users.noreply.github.com>
2023-08-11 07:02:46 +0100

    Create Twitter.js

---
+++ 3_Big_O/Twitter.js
@@ -0,0 +1,24 @@
+// find 1st, Nth...
+
+const arr = ["daybefore", "yesterday", "today"];
+
+arr[0];//O(1)
+
+arr[arr.length - 1];//O(1)
+
+
+// ..................
+
+const obj = [{
+    tweet: "daybefore",
+    date: 2012
+}, {
+    tweet: "yesterday",
+    date: 2014
+}, {
+    tweet: "today",
+    date: 2018
+}];
+
+
+"god damn it".length //O(n)
