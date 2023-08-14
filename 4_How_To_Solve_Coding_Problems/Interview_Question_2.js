6678f14fbaa0f39778918761fea8bc6a2c696715
chiazam <54377599+chiazam@users.noreply.github.com>
2023-08-14 14:47:38 +0100

    Update Interview_Question_2.js

--- 4_How_To_Solve_Coding_Problems/Interview_Question_2.js
+++ 4_How_To_Solve_Coding_Problems/Interview_Question_2.js
@@ -5,24 +5,24 @@ let ar2 = ["z", "y", "x"];
 
 let ar3 = ["z", "y", "f"];
 
-let containCommonItems2 = (arr1, arr2) => {
+let containCommonItems2 = (arr) => {
 
     let map = {};
 
-    for (let i = 0; i < arr1.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
 
         if (!map[i]) {
 
-            let item = arr1[i];
+            let item = ar2[i];
 
             map[item] = true;
         }
 
     }
 
-    for (let j = 0; j < arr2.length; j++) {
+    for (let j = 0; j < ar2.length; j++) {
 
-        if (map[arr2[j]]) {
+        if (map[ar2[j]]) {
 
             return true;
 
@@ -32,6 +32,10 @@ let containCommonItems2 = (arr1, arr2) => {
 
 } // O(n^2)
 
-containCommonItems2(ar1, ar2); // true
+containCommonItems2(ar1); // true
 
-containCommonItems2(ar1, ar3); // false
+let containCommonItems3 = (arr1, arr2) => {
+
+    return arr1.some(item => (arr2.include(item)));
+
+} // O(n^2)
