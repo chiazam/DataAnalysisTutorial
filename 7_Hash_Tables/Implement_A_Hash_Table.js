d4b1dedb6aedb030a495c8ee13073d37705ed050
chiazam <54377599+chiazam@users.noreply.github.com>
2023-08-21 20:23:08 +0100

    Update Implement_A_Hash_Table.js

--- 7_Hash_Tables/Implement_A_Hash_Table.js
+++ 7_Hash_Tables/Implement_A_Hash_Table.js
@@ -13,9 +13,7 @@ class HashTable {
         for (let i = 0; i < key.length; i++) {
             const element = key[i];
 
-            hash = (hash + key.charCodeAt(i) * i) %;
-
-            this.data.length
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
 
         }
 
@@ -23,12 +21,51 @@ class HashTable {
 
     }
 
+    set(key, value) {
+
+        let address = this._hash(key);
+
+        if (!this.data[address]) {
+
+            this.data[address] = [];
+
+        }
+
+        this.data[address].push([key, value]);
+
+    }
+
+    set(key) {
+
+        let address = this._hash(key);
+
+        let currentbucket = this.data[address];
+
+        if (currentbucket) {
+
+            for (let i = 0; i < currentbucket.length; i++) {
+
+                if (currentbucket[i][0] == key) {
+
+                    return currentbucket[i][1];
+
+                }
+
+            }
+
+        }
+
+        return undefined;
+
+
+    }
 
 }
 
 let hasher = new HashTable(50);
 
 hasher.set("grapes", 1000);
+hasher.set("apples", 1000);
 
 hasher.get("grapes");
 
